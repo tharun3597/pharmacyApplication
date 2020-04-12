a5d20b4775b077447baa193e76ee029231c5cf15
tharun3597 <tharun3597@gmail.com>
2020-04-13 00:24:55 +0530

    Initial Commit

---
+++ src/test/java/com/billing/pharmacyApplication/PharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.pharmacyApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
