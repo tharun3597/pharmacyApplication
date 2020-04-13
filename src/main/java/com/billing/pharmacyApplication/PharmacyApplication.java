911d27c29f5fad0a8590d98b86acfe12bc0ec8c5
tharun3597 <tharun3597@gmail.com>
2020-04-14 01:31:33 +0530

    Adding ComponentScan property

--- src/main/java/com/billing/pharmacyApplication/PharmacyApplication.java
+++ src/main/java/com/billing/pharmacyApplication/PharmacyApplication.java
@@ -1,9 +1,12 @@
 package com.billing.pharmacyApplication;
 
+import com.billing.pharmacyApplication.Controller.SampleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = SampleController.class)
 public class PharmacyApplication {
 
     public static void main(String[] args) {
